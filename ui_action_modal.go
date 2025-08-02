46c8d83a1dbee2a7be7716c31b23a9a547cbde86
denmo32 <sf2library@gmail.com>
2025-08-02 18:15:03 +0900

    feat: 行動選択モーダルのボタンを、原作に似せた配置へと変更

--- ui_action_modal.go
+++ ui_action_modal.go
@@ -8,81 +8,185 @@ import (
 
 func createActionModalUI(
 	vm *ActionModalViewModel,
-	uiFactory *UIFactory, // UIFactoryを追加
+	uiFactory *UIFactory,
 	eventChannel chan UIEvent,
 ) widget.PreferredSizeLocateableWidget {
 	c := uiFactory.Config.UI
 
-	// ボタンウィジェットのスライスを作成
-	buttons := []widget.PreferredSizeLocateableWidget{}
+	// 各パーツカテゴリのボタンを格納するマップ
+	partButtons := make(map[PartSlotKey][]widget.PreferredSizeLocateableWidget)
 
 	if len(vm.Buttons) == 0 {
-		buttons = append(buttons, widget.NewText(
+		// ボタンがない場合のメッセージ
+		noPartsText := widget.NewText(
 			widget.TextOpts.Text(uiFactory.MessageManager.FormatMessage("ui_no_parts_available", nil), uiFactory.Font, c.Colors.White),
-		))
-	} else {
-		for _, buttonVM := range vm.Buttons {
-			buttonText := fmt.Sprintf("%s (%s)", buttonVM.PartName, buttonVM.PartCategory)
-			buttonTextColor := &widget.ButtonTextColor{Idle: c.Colors.White}
-
-			actionButton := uiFactory.NewCyberpunkButton(
-				buttonText,
-				buttonTextColor,
-				func(args *widget.ButtonClickedEventArgs) {
-					eventChannel <- ClearCurrentTargetUIEvent{}
-
-					eventChannel <- ActionConfirmedUIEvent{
-						ActingEntityID:    vm.ActingEntityID,
-						SelectedPartDefID: buttonVM.SelectedPartDefID,
-						SelectedSlotKey:   buttonVM.SlotKey,
-						TargetEntityID:    buttonVM.TargetEntityID,
-						TargetPartSlot:    buttonVM.TargetPartSlot,
-					}
-				},
-				func(args *widget.ButtonHoverEventArgs) {
-					switch buttonVM.PartCategory {
-					case CategoryRanged:
-						if buttonVM.TargetEntityID != 0 {
-							eventChannel <- TargetSelectedUIEvent{
-								ActingEntityID:  vm.ActingEntityID,
-								SelectedSlotKey: buttonVM.SlotKey,
-								TargetEntityID:  buttonVM.TargetEntityID,
-								TargetPartSlot:  "",
-							}
-						}
-					case CategoryIntervention:
-						// 介入の場合はターゲット表示なし
-					default:
-						// 格闘など、他のカテゴリでターゲット表示が必要な場合はここに追加
-					}
-				},
-				func(args *widget.ButtonHoverEventArgs) {
-					eventChannel <- ClearCurrentTargetUIEvent{}
-				},
-			)
-			buttons = append(buttons, actionButton)
-		}
+		)
+		// 中央に配置するためのコンテナ
+		centeredTextContainer := widget.NewContainer(
+			widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+		)
+		centeredTextContainer.AddChild(noPartsText)
+		return centeredTextContainer
 	}
 
-	// コンテンツを格納するコンテナを作成
+	for _, buttonVM := range vm.Buttons {
+		buttonText := fmt.Sprintf("%s (%s)", buttonVM.PartName, buttonVM.PartCategory)
+		buttonTextColor := &widget.ButtonTextColor{Idle: c.Colors.White}
+
+		actionButton := uiFactory.NewCyberpunkButton(
+			buttonText,
+			buttonTextColor,
+			func(args *widget.ButtonClickedEventArgs) {
+				eventChannel <- ClearCurrentTargetUIEvent{}
+
+				eventChannel <- ActionConfirmedUIEvent{
+					ActingEntityID:    vm.ActingEntityID,
+					SelectedPartDefID: buttonVM.SelectedPartDefID,
+					SelectedSlotKey:   buttonVM.SlotKey,
+					TargetEntityID:    buttonVM.TargetEntityID,
+					TargetPartSlot:    buttonVM.TargetPartSlot,
+				}
+			},
+			func(args *widget.ButtonHoverEventArgs) {
+				switch buttonVM.PartCategory {
+				case CategoryRanged:
+					if buttonVM.TargetEntityID != 0 {
+						eventChannel <- TargetSelectedUIEvent{
+							ActingEntityID:  vm.ActingEntityID,
+							SelectedSlotKey: buttonVM.SlotKey,
+							TargetEntityID:  buttonVM.TargetEntityID,
+							TargetPartSlot:  "",
+						}
+					}
+				case CategoryIntervention:
+					// 介入の場合はターゲット表示なし
+				default:
+					// 格闘など、他のカテゴリでターゲット表示が必要な場合はここに追加
+				}
+			},
+			func(args *widget.ButtonHoverEventArgs) {
+				eventChannel <- ClearCurrentTargetUIEvent{}
+			},
+		)
+		partButtons[buttonVM.SlotKey] = append(partButtons[buttonVM.SlotKey], actionButton)
+	}
+
+	// 最外側のコンテナ（中央配置用）
+	outerContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+
+	// メインのコンテンツコンテナ
 	contentContainer := widget.NewContainer(
 		widget.ContainerOpts.Layout(widget.NewRowLayout(
 			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
-			widget.RowLayoutOpts.Padding(widget.NewInsetsSimple(15)), // パディングはここで設定
 			widget.RowLayoutOpts.Spacing(c.ActionModal.ButtonSpacing),
+			widget.RowLayoutOpts.Padding(widget.NewInsetsSimple(15)),
 		)),
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+			HorizontalPosition: widget.AnchorLayoutPositionCenter,
+			VerticalPosition:   widget.AnchorLayoutPositionCenter,
+		})),
 	)
 
-	// タイトルを追加
+	// タイトルセクション
 	title := widget.NewText(
 		widget.TextOpts.Text(uiFactory.MessageManager.FormatMessage("ui_action_select_title", map[string]interface{}{"MedarotName": vm.ActingMedarotName}), uiFactory.Font, c.Colors.White),
 	)
 	contentContainer.AddChild(title)
 
-	// ボタンを追加
-	for _, btn := range buttons {
-		contentContainer.AddChild(btn)
-	}
+	// パーツ配置用のメインコンテナ（3行1列のグリッド）
+	partsContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewGridLayout(
+			widget.GridLayoutOpts.Columns(1),
+			widget.GridLayoutOpts.Stretch([]bool{true}, []bool{false, true, false}), // 中央の行をストレッチ
+			widget.GridLayoutOpts.Spacing(0, c.ActionModal.ButtonSpacing*2),
+		)),
+	)
 
-	return contentContainer
+	// 1行目: 頭部パーツ（中央配置）
+	headRowContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+
+	headPartsContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionHorizontal),
+			widget.RowLayoutOpts.Spacing(c.ActionModal.ButtonSpacing),
+		)),
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+			HorizontalPosition: widget.AnchorLayoutPositionCenter,
+			VerticalPosition:   widget.AnchorLayoutPositionCenter,
+		})),
+	)
+
+	for _, btn := range partButtons[PartSlotHead] {
+		headPartsContainer.AddChild(btn)
+	}
+	headRowContainer.AddChild(headPartsContainer)
+	partsContainer.AddChild(headRowContainer)
+
+	// 2行目: 空のスペーサー
+	spacerContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.MinSize(1, 50)), // 最小高さを設定
+	)
+	partsContainer.AddChild(spacerContainer)
+
+	// 3行目: 腕パーツ（左右分割）
+	armRowContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewGridLayout(
+			widget.GridLayoutOpts.Columns(2),
+			widget.GridLayoutOpts.Stretch([]bool{true, true}, []bool{true}),
+			widget.GridLayoutOpts.Spacing(c.ActionModal.ButtonSpacing*4, 0), // 左右の間隔を広く
+		)),
+	)
+
+	// 右腕パーツ（1列目、右寄せ）
+	rightArmContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+	rightArmPartsContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Spacing(c.ActionModal.ButtonSpacing),
+		)),
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+			HorizontalPosition: widget.AnchorLayoutPositionEnd,
+			VerticalPosition:   widget.AnchorLayoutPositionCenter,
+		})),
+	)
+	for _, btn := range partButtons[PartSlotRightArm] {
+		rightArmPartsContainer.AddChild(btn)
+	}
+	rightArmContainer.AddChild(rightArmPartsContainer)
+	armRowContainer.AddChild(rightArmContainer)
+
+	// 左腕パーツ（2列目、左寄せ）
+	leftArmContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewAnchorLayout()),
+	)
+	leftArmPartsContainer := widget.NewContainer(
+		widget.ContainerOpts.Layout(widget.NewRowLayout(
+			widget.RowLayoutOpts.Direction(widget.DirectionVertical),
+			widget.RowLayoutOpts.Spacing(c.ActionModal.ButtonSpacing),
+		)),
+		widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+			HorizontalPosition: widget.AnchorLayoutPositionStart,
+			VerticalPosition:   widget.AnchorLayoutPositionCenter,
+		})),
+	)
+	for _, btn := range partButtons[PartSlotLeftArm] {
+		leftArmPartsContainer.AddChild(btn)
+	}
+	leftArmContainer.AddChild(leftArmPartsContainer)
+	armRowContainer.AddChild(leftArmContainer)
+
+	partsContainer.AddChild(armRowContainer)
+	contentContainer.AddChild(partsContainer)
+
+	// 最外側のコンテナにメインコンテンツを追加
+	outerContainer.AddChild(contentContainer)
+
+	return outerContainer
 }
